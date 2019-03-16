fd7400e556e3c147ffc10e0b4c7e17cad3c1b35d
pbarco <--global>
2019-03-16 13:04:24 -0300

    Creando un mini webserver con Node.js

--- Clase 8/App/index.js
+++ Clase 8/App/index.js
@@ -1,2 +1,17 @@
-var saludo = "Hola al mundo desde <strong>Node.js</strong>!";
-console.log (saludo)
+//ES8: import http from "./http" (es lo que se viene en la proxima version)
+let http = require("http")
+
+http.createServer(function(peticion, respuesta){
+
+	let saludo = "Hola al mundo desde <strong>Node.js</strong>!";
+	
+	respuesta.setHeader ("Content-Type", "text/html")
+
+	respuesta.write (saludo)
+	
+	respuesta.end("Usted quiere este recurso: " + peticion.url)
+
+}).listen(2000)
+
+
+
