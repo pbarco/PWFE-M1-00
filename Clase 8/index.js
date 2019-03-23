a137ee3d52765943d51182a9c0fd0e49b16d0ef5
pbarco <--global>
2019-03-23 14:31:08 -0300

    Formulario, Peticiones, Datos y LokiJS

--- Clase 8/index.js
+++ Clase 8/index.js
@@ -1,15 +1,49 @@
 const http = require("http")
 const fs = require("fs")
 const path = require("path")
+const form = require("querystring")
+const loki = require("lokijs")
 
 const port = 80
 
+let noticias = null;
+
+let db = new loki('noticias.json', {
+    autoload: true,
+    autosave: true, 
+    autosaveInterval: 4000,
+    autoloadCallback : function(){
+    	noticias = db.getCollection("noticias")
+    	if( noticias === null){
+    		noticias = db.addCollection ("noticias")
+    	}
+    }
+ })
+
+
 http.createServer(function(request, response){
-	let dir = "public/" // Carpeta del proyecto
+	let dir = "./public/" // Carpeta del proyecto
 
 	//let file = request.url // Archivo solicitado
 
 	let file = (request.url == "/") ? "index.html" : request.url// Operador Ternario
+
+	if( request.method == "POST" && file == "/enviar"){
+		//ACA HAY QUE PROCESAR LOS DATOS DEL FORMULARIO .... 
+
+		request.on("data", function(body){
+
+			let datos = body.toString()
+				datos = form.parse(datos)
+
+			noticias.insert (datos)
+
+			console.log( datos )
+
+			response.end("Mirar el archivo noticias.json")
+		})
+	} 
+
 	let ext = String ( path.extname(file) ).toLowerCase() //Extensiones ".html", ".css","js", Etc..
 	let tipos = {
 			".html"	: "text/html",
